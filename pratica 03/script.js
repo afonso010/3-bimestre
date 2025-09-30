dfc74452099fb923234ef29fd799ecdd9085b681
Joao afonso <afonsojoao168@gmail.com>
2025-09-30 15:38:39 -0300

    Add files via upload

---
+++ pratica 03/script.js
@@ -0,0 +1,18 @@
+// arrays -> vetores, matrizes
+// estrutura de dados para armazenar um conjunto de elementos
+
+let frutas = ["banana", "maçã","melancia"]; // array taamanho 3
+console.log(frutas);
+console.log(frutas[1]);
+
+let numeros = new Array(10,2,6,7);
+console.log(numeros);
+
+let arrayvazio = [];
+console.log(arrayvazio);
+arrayvazio[0] = "neymar";
+console.log(arrayvazio);
+
+//Tamanho de array -> length
+let dados = [32,4,80,6]; //tamamnho 5 -> length 5 
+console.log(dados.length);
